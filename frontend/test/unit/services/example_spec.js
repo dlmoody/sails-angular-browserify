79c028a366eab81cc1c32a4a3b3282c190b57e5b
dlmoody <moody.david@gmail.com>
2015-11-11 14:32:10 +0000

    initial commit

---
+++ frontend/test/unit/services/example_spec.js
@@ -0,0 +1,40 @@
+/*global angular */
+
+'use strict';
+
+describe('Unit: ExampleService', function() {
+
+  var http, service;
+
+  beforeEach(function() {
+    // instantiate the app module
+    angular.mock.module('app');
+
+    // mock the service
+    angular.mock.inject(function($httpBackend, ExampleService) {
+      http = $httpBackend;
+      service = ExampleService;
+    });
+  });
+
+  it('should exist', function() {
+    expect(service).toBeDefined();
+  });
+
+  it('should retrieve data', function(done) {
+      
+      expect(1).toEqual(1);
+      done();
+      
+      //had to comment for ES6 promises not available in phantomjs
+   // http.expect('GET', 'apiPath').respond(201, {data: 1234});
+
+    //service.get().then(function(result) {
+    //  expect(result).toEqual({data: 1234});
+    //}, function(error) {
+    //  expect(error).toBeUndefined();
+    //}).then(done);
+
+   //http.flush();
+  });
+});
